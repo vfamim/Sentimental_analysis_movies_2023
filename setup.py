362e32eca0ef7ffcb8b8c46be745bf577736d7fd
vfamim <vfamim@gmail.com>
2023-09-11 15:15:20 -0300

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Project aims to analyze feeling about films of 2023',
+    author='vfamim',
+    license='MIT',
+)
